d87b7b4f87565947d0a9f1fe6d195f1b3e13c9e4
giang.pham <wade712vn@gmail.com>
2020-01-08 02:58:24 +0700

    First code commit

---
+++ dotnet-code-challenge/BetEasyConsoleApplication.cs
@@ -0,0 +1,15 @@
+﻿namespace dotnet_code_challenge
+{
+    public class BetEasyConsoleApplication : IApplication
+    {
+        public BetEasyConsoleApplication()
+        {
+            
+        }
+
+        public void Run(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ dotnet-code-challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace dotnet_code_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddOptions()
+                .AddSingleton<IApplication, BetEasyConsoleApplication>()
+                .BuildServiceProvider();
+
+
+            serviceProvider.GetService<IApplication>().Run(args);
+        }
+    }
+}
